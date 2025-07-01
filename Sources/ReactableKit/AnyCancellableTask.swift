15a0f14b256e9e22f3cb18d4cccaad71c5658e53
JeeHoon Son <tigerjs94@gmail.com>
2025-07-01 11:26:47 +0900

    added AnyCancellableTask

---
+++ Sources/ReactableKit/AnyCancellableTask.swift
@@ -0,0 +1,40 @@
+//
+//  AnyCancellableTask.swift
+//  ReactableKit
+//
+//  Created by Jeehoon Son on 7/1/25.
+//
+
+import Foundation
+
+public protocol AnyCancellableTask {
+    func cancel()
+}
+
+extension Task: AnyCancellableTask {}
+
+public final class TaskCancellableBag {
+    private var tasks: [any AnyCancellableTask] = []
+    
+    public init() {}
+
+    public func add(task: any AnyCancellableTask) {
+        self.tasks.append(task)
+    }
+
+    public func cancel() {
+        self.tasks.forEach { $0.cancel() }
+        self.tasks.removeAll()
+    }
+    
+    deinit {
+        self.cancel()
+    }
+}
+
+
+extension Task {
+    public func store(in bag: TaskCancellableBag) {
+        bag.add(task: self)
+    }
+}
